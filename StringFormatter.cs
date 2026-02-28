e02ebc8698280a8ce53c94187dfacb97743f84b9
GordonData.com <GordonJHCHEN@live.com>
2026-03-01 10:50:08 +1100

    added XML doc comments fixed typo in ToCommaSepatatedList removed parameter "quote" which must always be double quotes per RFC 4180 added code to check inputs for abnormal values made minor changes: removed redundant code from StringBuilder initialisation, change loop to start from 0 applied important fix: double quote embedded in a field must be represented as two double quotes

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -2,33 +2,42 @@
 
 namespace PointsBet_Backend_Online_Code_Test
 {
-
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
     public class StringFormatter
     {
-
-        //Code to improve
-        public static string ToCommaSepatatedList(string[] items, string quote)
+        // 1. Important to add XML doc comments as below for IntelliSense, for maintenance, and for DocFX to generate documentation.
+        // 2. https://en.wikipedia.org/wiki/Comma-separated_values
+        //    If a technical standard already exists, it is important to abide by it; otherwise, interoperability will be lost.
+        //    Therefore, the parameter "quote" should be avoided — it must always be double quotes per RFC 4180.
+        
+        /// <summary>
+        /// Formats an array of strings into a comma-separated value (CSV) string.
+        /// </summary>
+        /// <param name="items">The array of strings to format.</param>
+        /// <returns>The comma-separated value; null if the array is null, string.Empty if the array is empty.</returns>
+        public static string ToCommaSeparatedList(string[] items) // ToCommaSepatatedList had a typo
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
+            // Always important to check inputs for abnormal values
+            if (items == null)
+                return null;
+            if (items.Length == 0)
+                return string.Empty;
 
-            if (items.Length > 1)
+            const char comma = ',', quote = '"';
+            StringBuilder sb = new StringBuilder(); // No need to initialise StringBuilder with a value, especially when it results in redundant code. 
+                                                    // sb is better than qry for a StringBuilder variable, unless it is used to build a query string. 
+            for (int i = 0; i < items.Length; i++)  // In C#, loop normally starts from 0
             {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
+                if (i > 0)
+                    sb.Append(comma);
+
+                // RFC 4180: Any field may be enclosed in double quotes 
+                sb.Append(quote);
+                // RFC 4180: If a field is enclosed in double quotes, then a double quote embedded in the field must be represented by a sequence of two double quotes
+                sb.Append(items[i].Replace(quote.ToString(), string.Concat(quote, quote)));
+                sb.Append(quote);
             }
 
-            return qry.ToString();
+            return sb.ToString();
         }
     }
 }
